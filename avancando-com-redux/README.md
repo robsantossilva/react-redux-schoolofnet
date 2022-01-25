21c6070bd851eee934c96fc37022792d6c379c8a
Robson Silva <robsantossilva@gmail.com>
2022-01-25 18:22:52 -0300

    end avancando-com-redux

--- avancando-com-redux/README.md
+++ avancando-com-redux/README.md
@@ -1,2 +1,10 @@
 ### Avançando com Redux
 
+**Actions** são recebidas pelos **Dispatches**
+**Dispatches** acionam os **Reducers**
+**Reducers** atualizam a **Store**
+
+**Actions** Definem o tipo da ação e as informações a serem atualizadas na Store
+**Reducers** são responsaveis por filtrar a ação e tratar as informações que serão enviadas para o Store
+**Store** Mantem a fonte unica de informação
+
--- avancando-com-redux/src/components/Counter.js
+++ avancando-com-redux/src/components/Counter.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { clickCounterMoreAction, clickCounterLessAction } from '../store';
 
 class Counter extends Component {
     constructor(props){
@@ -8,11 +10,23 @@ class Counter extends Component {
 
     render() {
         return (
-            <div>
+            <div style={{marginLeft: '30px'}}>
+                <h3>
+                    Dispatch
+                </h3>
                 <div>
                     <button type='button' onClick={() => this.props.dispatch({type: 'DECREMENT'})}>-</button>
                     <h5>{this.props.total}</h5>
                     <button type='button' onClick={() => this.props.dispatch({type: 'INCREMENT'})}>+</button>
+                </div>
+                <hr />
+                <h3>
+                    Action
+                </h3>
+                <div>
+                    <button type='button' onClick={() => this.props.clickCounterLessAction()}>-</button>
+                    <h5>{this.props.total}</h5>
+                    <button type='button' onClick={() => this.props.clickCounterMoreAction()}>+</button>
                 </div>
             </div>
         );
@@ -25,11 +39,11 @@ const mapStateToProps = (state) => {
     }
 }
 
-// const mapDispatchToProps = (dispatch) => {
-//     console.log(dispatch);
-//     return {
+const mapDispatchToProps = (dispatch) => {
+    return bindActionCreators({
+        clickCounterMoreAction,
+        clickCounterLessAction
+    }, dispatch);
+}
 
-//     }
-// }
-
-export default connect(mapStateToProps)(Counter);
+export default connect(mapStateToProps, mapDispatchToProps)(Counter);
